fb3634e8bc80679dda0a7fd3f4dce4fa4175a88a
Zia Fadhilah <ziafadhilah98@gmail.com>
2020-12-26 09:45:49 +0700

    woi

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,23 +9,29 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-    <title>@yield('title') : Perpustakaan</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="shortcut icon" href="{{asset('img/logo.png')}}" type="image/x-icon">
+    <title>@yield('title') : SMK Negeri 2 Bandung</title>
     <style>
         .zoom {
             transition: transform .1s;
             /* Animation */
-            margin: 0 auto;
+            /* margin: 0 auto; */
         }
 
         .zoom:hover {
-            transform: scale(1.2);
+            transform: scale(0.9);
             /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+        }
+        :root{
+            --animate-duration: 800ms;
+            --animate-delay: 0.9s;
         }
     </style>
 </head>
 
 <body>
-    <div class="container">
+    <!-- <div class="container">
         <div class="d-flex bd-highlight">
             <div class="p-2 bd-highlight">
                 <div class="btn-group dropdown">
@@ -40,8 +46,8 @@
                 </div>
             </div>
         </div>
-    </div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    </div> -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container">
             <a href="{{url('/')}}">
                 <div class="zoom">
@@ -54,21 +60,34 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ml-auto">
                     <div class="zoom">
-                        <a class="nav-item nav-link" href="{{url('/')}}"><i class="fa fa-angle-right"></i> <b>Daftar Buku</b> <i class="fa fa-angle-left"></i></a>
+                        <a class="nav-item nav-link" href="{{url('/')}}">
+                            <h5 style="color: #f5ab35;">PROFIL SEKOLAH</h5>
+                        </a>
                     </div>
                     <div class="zoom">
-                        <a class="nav-item nav-link" href="{{url('/about')}}"><i class="fa fa-angle-right"></i> <b>Kategori</b> <i class="fa fa-angle-left"></i></a>
+                        <a class="nav-item nav-link" href="{{url('/kegiatan')}}">
+                            <h5 style="color: #f5ab35;">KEGIATAN</h5>
+                        </a>
+                    </div>
+                    <div class="zoom">
+                        <a class="nav-item nav-link" href="{{url('/kompetensi')}}">
+                            <h5 style="color: #f5ab35;">KOMPETENSI</h5>
+                        </a>
+                    </div>
+                    <div class="zoom">
+                        <a class="nav-item nav-link" href="{{url('/guru')}}">
+                            <h5 style="color: #f5ab35;">MANAJEMEN</h5>
+                        </a>
                     </div>
                 </div>
             </div>
     </nav>
-    <p></p>
 
     @yield('container')
 
-    <div class="card-footer mt-3 bg-primary">
+    <div class="card-footer mt-3 bg-dark">
         <div class="container">
-            &copy 2020 Zia Fadhilah - All right reserved!
+            <h5 style="color: #f5ab35;">&copy 2020 SMK Negeri 2 - All right reserved!</h5>
         </div>
     </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,14 +13,20 @@
         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
+        <div class="carousel-caption d-none d-md-block">
+            <h1 style="color: aqua;" class="animate__animated animate__fadeInLeft">SELAMAT DATANG DI</h1>
+            <p>
+                <h5 style="color: #f5ab35;" class="animate__animated animate__fadeInRight">WEBSITE SMK NEGERI 2 KOTA BANDUNG</h5>
+            </p>
+        </div>
         <div class="carousel-item active">
-            <img src="{{asset('img/1.jpg')}}" class="d-block w-100" alt="first">
+            <img src="{{asset('img/1.jpg')}}" class="d-block w-100 animate__animated animate__fadeIn animate__delay-1s" alt="first">
         </div>
         <div class="carousel-item">
-            <img src="{{asset('img/2.jpg')}}" class="d-block w-100" alt="second">
+            <img src="{{asset('img/2.jpg')}}" class="d-block w-100 animate__animated animate__fadeIn animate__delay-1s" alt="second">
         </div>
         <div class="carousel-item">
-            <img src="{{asset('img/3.jpg')}}" class="d-block w-100" alt="third">
+            <img src="{{asset('img/3.jpg')}}" class="d-block w-100 animate__animated animate__fadeIn animate__delay-1s" alt="third">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
@@ -31,6 +37,29 @@
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
+</div>
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-sm-5 col-md-5 mt-3">
+            <img src="{{asset('img/hasan-iskandar.png')}}" width="100%">
+        </div>
+        <div class="col-sm-7 col-md-7 mt-3">
+            <h1>SAMBUTAN KEPALA SEKOLAH</h1>
+            <p class="text-muted">
+                <h5>
+                    Assalamualaikum Warrahmatullahi Wabarrakatuh.
+                </h5>
+            </p>
+            <p class="text-muted">
+                Selamat datang di website SMK Negeri 2 Bandung.
+            </p>
+            <p class="text-muted">
+                Di SMKN 2 Bandung setiap siswa memiliki hak untuk berprestasi dan mendapatkan pelayanan yang baik. Karena kami memandang ini semua adalah amanah yang akan kami pertanggung jawabkan di hadapan Allah SWT. Tentu hal ini semakin terasa mudah dengan adanya kerjasama dari para orang tua siswa dalam menjalankan program sekolah.
+            <br>
+                Sekali lagi kami ucapkan selamat datang di SMKN 2 Bandung. Marilah bekerjasama agar anak-anak kita dapat berkembang dengan baik sehingga tumbuh menjadi generasi yang berakhlakul karimah dan cerdas.
+            </p>
+        </div>
+    </div>
 </div>
 <div class="container mt-3">
     <div class="row">
